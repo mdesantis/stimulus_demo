b068bcb20a9028524346fd5e95eac48a829525c7
Maurizio De Santis <desantis.maurizio@gmail.com>
2018-02-17 16:06:32 +0100

    Add console logs for Channel channel

--- app/assets/javascripts/channels/channel.js
+++ app/assets/javascripts/channels/channel.js
@@ -1,10 +1,16 @@
 App.channel = App.cable.subscriptions.create('ChannelChannel', {
   // Called when the subscription is ready for use on the server
-  connected() {},
+  connected() {
+    console.log('ChannelChannel connected')
+  },
 
   // Called when the subscription has been terminated by the server
-  disconnected() {},
+  disconnected() {
+    console.log('ChannelChannel disconnected')
+  },
 
   // Called when there's incoming data on the websocket for this channel
-  received(_data) {},
+  received(data) {
+    console.log('ChannelChannel received', data)
+  },
 })
