7af9b32b962b50b78a4e95a046f2aead18cfb29a
Maurizio De Santis <desantis.maurizio@gmail.com>
2018-02-17 19:34:31 +0100

    bundle update and install scss linter

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,11 @@
 class Message < ApplicationRecord
   validates_presence_of :text
 
-  after_commit { MessageCreatedJob.perform_later(self) }
+  after_commit :perform_message_created_job, on: :create
+
+  private
+
+  def perform_message_created_job
+    MessageCreatedJob.perform_later(self)
+  end
 end
