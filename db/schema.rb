fedb14bebdfdd3e6c107dae9e5691a2a7cf6b287
Maurizio De Santis <desantis.maurizio@gmail.com>
2018-02-23 10:10:26 +0100

    Switch to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2018_01_07_112431) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -33,7 +36,11 @@ ActiveRecord::Schema.define(version: 2018_01_07_112431) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-# Could not dump table "messages" because of following StandardError
-#   Unknown type 'cidr' for column 'author_ip'
+  create_table "messages", force: :cascade do |t|
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.cidr "author_ip"
+  end
 
 end
