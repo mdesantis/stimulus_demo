4bfdb01781be307cc30f47a571104c7b84debcd9
Maurizio De Santis <desantis.maurizio@gmail.com>
2018-02-23 15:16:32 +0100

    Remove commented lines

--- app/javascript/packs/controllers/channel_controller.js
+++ app/javascript/packs/controllers/channel_controller.js
@@ -2,15 +2,11 @@ import { Controller } from 'stimulus'
 
 export default class extends Controller {
   connect() {
-    // console.log('Hello, Stimulus!', this.element);
-    // const channelEl = document.getElementById('channel')
-    // App.stimulus.getControllerForElementAndIdentifier(channelEl, 'channel').createMessage(data.message)
     document.addEventListener('cable:channel:messagecreated', this.constructor.createMessage)
   }
 
   static createMessage(event) {
     console.log('createMessage', event)
-    // const message = event.detail.message
     const { message } = event.detail
     document.getElementById('channel-messages').insertAdjacentHTML('beforeend', message)
   }
