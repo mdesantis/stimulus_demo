0fb8fee2d17cfee2cb00a495dcf4e0e7bec9e26e
Maurizio De Santis <desantis.maurizio@gmail.com>
2018-03-02 16:15:49 +0100

    Refactor message form javascript controller

--- app/javascript/packs/controllers/message_form_controller.js
+++ app/javascript/packs/controllers/message_form_controller.js
@@ -2,14 +2,31 @@ import { Controller } from 'stimulus'
 
 export default class extends Controller {
   connect() {
-    this.element.addEventListener('ajax:success', this.constructor.ajaxSuccess)
+    this.listeners = []
+
+    this.addEventListener(this.element, 'ajax:success', this.ajaxSuccess)
   }
 
   disconnect() {
-    this.element.removeEventListener('ajax:success', this.constructor.ajaxSuccess)
+    this.removeEventListeners()
   }
 
-  static ajaxSuccess(_event) {
-    document.getElementById('message_text').value = ''
+  ajaxSuccess(_event) {
+    this.textarea().value = ''
+  }
+
+  textarea() {
+    return this.element.querySelector('[name="message[text]"]')
+  }
+
+  addEventListener(element, eventName, listener) {
+    element.addEventListener(eventName, listener.bind(this))
+    this.listeners.push([element, eventName, listener])
+  }
+
+  removeEventListeners() {
+    this.listeners.forEach(([element, eventName, listener]) => {
+      element.removeEventListener(eventName, listener)
+    })
   }
 }
