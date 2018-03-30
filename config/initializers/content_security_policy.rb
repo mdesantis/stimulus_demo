95b13b72d373d7a68cfd862664e6089f3e95dbbb
Maurizio De Santis <desantis.maurizio@gmail.com>
2018-03-30 17:45:30 +0200

    Enable CSP config

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,21 +4,25 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-# Rails.application.config.content_security_policy do |policy|
-#   policy.default_src :self, :https
-#   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :data
-#   policy.object_src  :none
-#   policy.script_src  :self, :https
-#   policy.style_src   :self, :https, :unsafe_inline
-#   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
+Rails.application.config.content_security_policy do |policy|
+  policy.default_src :self, :https
+  policy.font_src    :self, :https, :data
+  policy.img_src     :self, :https, :data
+  policy.object_src  :none
+  # policy.script_src :unsafe_inline can be removed after the following commit gets included:
+  # https://github.com/rails/rails/commit/b2f0a8945956cd92dec71ec4e44715d764990a49
+  policy.script_src  :self, :https, :unsafe_inline
+  policy.style_src   :self, :https, :unsafe_inline
+  policy.connect_src :self,
+                     :https,
+                     *(%w[http://localhost:3035 ws://localhost:3000 ws://localhost:3035] if Rails.env.development?)
 
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
-# end
+  # Specify URI for violation reports
+  # policy.report_uri "/csp-violation-report-endpoint"
+end
 
 # If you are using UJS then enable automatic nonce generation
-# Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
+Rails.application.config.content_security_policy_nonce_generator = ->(_request) { SecureRandom.base64(16) }
 
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
