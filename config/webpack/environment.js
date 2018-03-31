2165aa499af061be605ed4916753d3a427d28b0c
Maurizio De Santis <desantis.maurizio@gmail.com>
2018-03-31 15:12:28 +0200

    Remove eslint-loader on production

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,13 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-environment.loaders.append('eslint', {
-  test: /\.js$/,
-  use: {
-    loader: 'eslint-loader',
-    options: {
-      failOnError: process.env.NODE_ENV !== 'production',
-    },
-  },
-})
-
 module.exports = environment
---
+++ config/webpack/loaders/eslint.js
@@ -0,0 +1,6 @@
+module.exports = {
+  test: /\.js$/,
+  use: {
+    loader: 'eslint-loader',
+  },
+}
