a05cc964c78c44cbbf0173be9a9ac8a709060ab4
Maurizio De Santis <desantis.maurizio@gmail.com>
2018-04-06 14:49:36 +0200

    Disable Rubocop rule at line level

--- db/migrate/20180309102546_create_users.rb
+++ db/migrate/20180309102546_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.citext :email,           null: false, index: { unique: true }
+      # Rubocop issue: https://github.com/bbatsov/rubocop/issues/5657
+      t.citext :email,           null: false, index: { unique: true } # rubocop:disable Layout/ExtraSpacing
       t.citext :username,                     index: { unique: true }
       t.string :password_digest, null: false
 
