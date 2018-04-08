202ae54c930753fb3f50d57e9935dd976aededc6
Maurizio De Santis <desantis.maurizio@gmail.com>
2018-04-08 16:36:07 +0200

    Add eslint Webpack plugin

---
+++ .eslintrc.js
@@ -0,0 +1,82 @@
+module.exports = {
+  "env": {
+    "es6": true
+  },
+  "extends": [
+    "airbnb-base",
+    "plugin:import/errors",
+    "plugin:import/warnings"
+  ],
+  "overrides": [
+    {
+      "files": [
+        "app/assets/javascripts/**/*.js",
+        "app/javascript/**/*.js"
+      ],
+      "env": {
+        "browser": true
+      },
+      "globals": {
+        "App": false,
+        "Rails": false
+      }
+    },
+    {
+      "files": [
+        "app/assets/javascripts/cable/cable.js"
+      ],
+      "globals": {
+        "ActionCable": false
+      }
+    }
+  ],
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "rules": {
+    "eqeqeq": [
+      "error",
+      "always"
+    ],
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "max-len": [
+      "error",
+      {
+        "code": 120
+      }
+    ],
+    "no-debugger": [
+      "warn"
+    ],
+    "no-unused-vars": [
+      "error",
+      {
+        "argsIgnorePattern": "^_"
+      }
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "strict": 0
+  },
+  "settings": {
+    "import/resolver": {
+      "webpack": {
+        "config": `config/webpack/${process.env.NODE_ENV || 'development'}.js`
+      }
+    }
+  }
+}
