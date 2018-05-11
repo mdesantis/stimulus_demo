10c9d327f350c6e6d7a082c56e73e3bb82b442b9
Maurizio De Santis <desantis.maurizio@gmail.com>
2018-05-11 12:43:27 +0200

    Add user creation to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.first_or_create! email: 'user@example.com', username: 'user', password: 'password'
 Channel.first_or_create_primary!
