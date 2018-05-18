5171711822e70a63821763562271b34f3d86896e
Maurizio De Santis <desantis.maurizio@gmail.com>
2018-05-18 18:17:17 +0200

    Replace message form textarea with Quill.js

--- app/javascript/controllers/client/channel/message_form_controller.js
+++ app/javascript/controllers/client/channel/message_form_controller.js
@@ -1,3 +1,5 @@
+import Quill from 'quill'
+
 import ApplicationController from 'controllers/application_controller'
 
 export default class extends ApplicationController {
@@ -7,10 +9,34 @@ export default class extends ApplicationController {
     super.connect()
 
     this.addEventListeners(
-      [this.textTarget, 'keyup', this.submitFormOnEnter],
-      [this.element, 'ajax:success', this.ajaxSuccess],
+      // [this.textTarget, 'keyup', this.submitFormOnEnter],
+      // [this.element, 'ajax:success', this.ajaxSuccess],
       this.controllerEventListener('client--channel', 'change:current', this.updateChannelId),
     )
+
+    this.initQuill()
+  }
+
+  initQuill() {
+    this.quill = new Quill(this.textTarget, { placeholder: 'Jot something down' })
+    console.log(this.quill)
+    this.quill.on('selection-change', (range, _oldRange, _source) => {
+      if (range) {
+        console.log(this, 'focus')
+        this.textEditorFocus()
+      } else {
+        console.log(this, 'blur')
+        this.textEditorBlur()
+      }
+    })
+  }
+
+  textEditorFocus() {
+    this.textTarget.classList.add('focus')
+  }
+
+  textEditorBlur() {
+    this.textTarget.classList.remove('focus')
   }
 
   updateChannelId(event) {
@@ -18,17 +44,17 @@ export default class extends ApplicationController {
     this.channelIdTarget.value = event.detail.controller.channelId
   }
 
-  ajaxSuccess(_event) {
-    this.textTarget.value = ''
-  }
+  // ajaxSuccess(_event) {
+  //   this.textTarget.value = ''
+  // }
 
-  submitFormOnEnter(event) {
-    if (this.constructor.isEnter(event)) {
-      Rails.fire(this.element, 'submit')
-    }
-  }
-
-  static isEnter(event) {
-    return event.keyCode === 13 && !event.shiftKey
-  }
+  // submitFormOnEnter(event) {
+  //   if (this.constructor.isEnter(event)) {
+  //     Rails.fire(this.element, 'submit')
+  //   }
+  // }
+  //
+  // static isEnter(event) {
+  //   return event.keyCode === 13 && !event.shiftKey
+  // }
 }
